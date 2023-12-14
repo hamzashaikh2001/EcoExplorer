46fd49a22a298f5c78b74165b12d305329320125
hamzashaikh2001 <hamzashaikh2001@hotmail.com>
2023-12-14 17:24:54 -0600

    Initialized Spring Boot

---
+++ ecoexplorer/src/main/java/com/hamzashaikh2001/ecoexplorer/EcoexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.hamzashaikh2001.ecoexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoexplorerApplication.class, args);
+	}
+
+}
---
+++ ecoexplorer/src/test/java/com/hamzashaikh2001/ecoexplorer/EcoexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzashaikh2001.ecoexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
